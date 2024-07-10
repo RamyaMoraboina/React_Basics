68d4d296f25142279da257ebfb759645b6591f7c
RamyaMoraboina <ramya89780@gmail.com>
2024-07-10 18:20:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import react from 'react';
+import './App.css';
 
 function App(){
-  return <h1>hello react</h1>
+
+  return (
+  <section className="posts-container">
+    <Profile/>
+  </section>
+  );
 }
+
+function Profile(){
+  return(
+    <article className="profile-card">
+      <img src="https://via.placeholder.com/150/b2fed" alt="image" />
+      <h2 className='title'>accusamus beatae ad facilis cum similique qui sunt  </h2>
+      <a href="https://via.placeholder.com/600/92c952" className='button'>Details</a>
+    </article>
+  )
+}
+
+export default App;
